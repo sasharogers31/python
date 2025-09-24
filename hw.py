832d85a81d44e6c94d65ffcb1185fc602c8b135f
sasharogers31 <135717085+sasharogers31@users.noreply.github.com>
2025-09-24 02:36:59 -0700

    Resolve issue with commas

--- hw.py
+++ hw.py
@@ -1,6 +1,6 @@
 <<<<<<< HEAD
 print("hello, liza ;>")
-print(“hello, slay king and queen”)
+print("hello, slay king and queen")
 print("Hello, D's")
 print("I'm Commander Shepard, and this is my favorite store on the citadel")
 =======
